cad1752d7010bcebb25a128dea05a9295399f044
Thijs Ubbink | Mobiel Bekeken <thijs@mobielbekeken.nl>
2026-05-04 08:41:06 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")        
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),        
         .package(url: "https://github.com/danielamitay/DACircularProgress.git", exact: "2.3.1"),
         .package(url: "https://github.com/facebookarchive/pop.git", exact: "1.0.11")
     ],
