a64b1b11ccde2c01274dd6716c4eacc54d66559f
Thijs Ubbink | Mobiel Bekeken <thijs@mobielbekeken.nl>
2026-05-04 08:16:08 +0200

    SPM support

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "IDMPhotoBrowser",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "IDMPhotoBrowser",
+            targets: ["IDMPhotoBrowser"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "IDMPhotoBrowser",
+            path: "."
+        )
+    ]
+)
