4985b4e72894f1d59ffd88a7f451b59dbfdc3265
Thijs Ubbink | Mobiel Bekeken <thijs@mobielbekeken.nl>
2026-05-04 08:21:06 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .target(
             name: "IDMPhotoBrowser",
-            path: "Classes"
+            path: "Classes",
+            publicHeadersPath: "."
         )
     ]
 )
