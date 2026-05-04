342f8410c71c9398578895c1c5b2977806babadf
Thijs Ubbink | Mobiel Bekeken <thijs@mobielbekeken.nl>
2026-05-04 08:39:48 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -12,6 +12,11 @@ let package = Package(
             targets: ["IDMPhotoBrowser"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")        
+        .package(url: "https://github.com/danielamitay/DACircularProgress.git", exact: "2.3.1"),
+        .package(url: "https://github.com/facebookarchive/pop.git", exact: "1.0.11")
+    ],
     targets: [
         .target(
             name: "IDMPhotoBrowser",
