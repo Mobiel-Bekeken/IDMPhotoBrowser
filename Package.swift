c4301aa17dbabe8a39813580380366b4d6453cd4
Thijs Ubbink | Mobiel Bekeken <thijs@mobielbekeken.nl>
2026-05-06 11:29:29 +0200

    fixes

--- Package.swift
+++ Package.swift
@@ -16,13 +16,23 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),        
         .package(url: "https://github.com/Mobiel-Bekeken/DACircularProgress.git", from: "2.3.2"),
-    ],
+        .package(url: "https://github.com/Mobiel-Bekeken/pop.git", from: "1.0.13"),    
+],
     targets: [
         .target(
             name: "IDMPhotoBrowser",
             dependencies: [
                 .product(name: "DACircularProgress", package: "DACircularProgress"),
-                .product(name: "SDWebImage", package: "SDWebImage")
+                .product(name: "SDWebImage", package: "SDWebImage"),
+                .product(name: "pop", package: "pop)
+            ],
+            exclude: [
+                "IDMPBLocalizations.bundle",
+                "IDMPhotoBrowser.bundle",
+            ],
+            resources: [
+                .process("IDMPBLocalizations.bundle"),
+                .process("IDMPhotoBrowser.bundle")
             ],
             path: "Classes",
             publicHeadersPath: "."
